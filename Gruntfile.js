9fdabd2f2c26a077f2e6e10d9e8684d6d83d9df1
raubas <r.svartholm@gmail.com>
2015-07-07 15:38:20 +0200

    bootstrap shells

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,24 @@ module.exports = function(grunt) {
               task: {
                 src: ['dev/*.html']
               }
-        }
+        },
+
+        githooks: {
+            all: {
+              
+              // Will bind the bower:install task 
+              // with a specific template 
+              'post-merge': {
+                taskNames: 'bower:install'
+              }
+            }
         
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-wiredep');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-githooks');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', [ 'wiredep' ]);
