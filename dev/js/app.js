2d3fd2120f88014fe3778977a86ebd8534a00c5b
raubas <r.svartholm@gmail.com>
2015-07-08 10:41:31 +0200

    structure in your face

--- dev/js/app.js
+++ dev/js/app.js
@@ -3,16 +3,4 @@ Parse.initialize("6xK5z0dd13fPSziUDvcLiZTEqjkRc5qQais6zUSo", "dgHEctNMXBRjHFAYSS
 var app = angular.module('myApp', ['ngAnimate', 'parse-angular']);
 
 
-app.controller('testController', function($scope){
-
-	// Queries
-	$scope.doQuery = function(){
-		var query = new Parse.Query("Schools");
-		query.find().then(function(result){
-	        $scope.schools = result;
-	});
-	}
-	
-
-});
 
