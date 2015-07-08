ca174dc4eb6e3aac4715a720f4910f11fcf2be17
Jonatan Moritz <jonatan.moritz@gmail.com>
2015-07-08 10:38:29 +0200

    structure

---
+++ dev/js/controllers/testController.js
@@ -0,0 +1,15 @@
+
+
+app.controller('testController', function($scope){
+
+	// Queries
+	$scope.doQuery = function(){
+		var query = new Parse.Query("Schools");
+		query.find().then(function(result){
+	        $scope.schools = result;
+	});
+	}
+	
+
+});
+
