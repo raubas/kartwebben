b592b24687049ace486f4b3445d04400aecb23da
Mikael Svärdström <mikael.svardstrom@gmail.com>
2015-07-08 12:21:07 +0200

    listar skolorna

--- dev/js/controllers/schoolController.js
+++ dev/js/controllers/schoolController.js
@@ -2,13 +2,25 @@
 
 app.controller('schoolCtrl', function($scope){
 
-	// Queries
-	$scope.doQuery = function(){
-		var query = new Parse.Query("Schools");
+	var query = new Parse.Query("Schools");
+		query.include('ContactPerson');
 		query.find().then(function(result){
 	        $scope.schools = result;
-	});
-	}
+	    });
+
+	var queryContact = new Parse.Query("ContactPerson");
+		queryContact.include('ContactPerson');
+		queryContact.find().then(function(result){
+	        $scope.contactPerson = result;
+	    });
+
+	// // Queries
+	// $scope.doQuery = function(){
+	// 	var query = new Parse.Query("Schools");
+	// 	query.find().then(function(result){
+	//         $scope.schools = result;
+	// });
+	// }
 	
 
 });
