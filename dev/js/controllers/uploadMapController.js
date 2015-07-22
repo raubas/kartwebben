e4fbbc16d6cf16dbc90f9c4ea9db06b06f7a79b1
Mikael Svärdström <mikael.svardstrom@gmail.com>
2015-07-22 16:24:20 +0200

    start save area

--- dev/js/controllers/uploadMapController.js
+++ dev/js/controllers/uploadMapController.js
@@ -3,8 +3,36 @@ app.controller('uploadMapCtrl', function($scope){
 	var query = new Parse.Query("Areas");
 		query.include("maps");
 		query.find().then(function(result){
-	        $scope.areeas = result;
+	        $scope.areas = result;
 	    });
+
+	$scope.displayMap = function($url){
+		return $url;
+	}
+
+	var queryMaps = new Parse.Query("Maps");
+		queryMaps.find().then(function(result){
+		$scope.maps = result;
+	});
+
+
+	$scope.saveArea = function(name, level, file){
+		var Areas = Parse.Object.extend("Areas");
+		var area = new Areas();
+		area.set("name",name);
+		area.save(null, {
+		  success: function(area) {
+		    // Execute any logic that should take place after the object is saved.
+		    alert('New object created with objectId: ' + area.id);
+		  },
+		  error: function(area, error) {
+		    // Execute any logic that should take place if the save fails.
+		    // error is a Parse.Error with an error code and message.
+		    alert('Failed to create new object, with error code: ' + error.message);
+		  }
+		});
+	};
+
 
 
 	// var queryContact = new Parse.Query("ContactPerson");
@@ -21,5 +49,4 @@ app.controller('uploadMapCtrl', function($scope){
 	// });
 	// }
 	
-
 });
