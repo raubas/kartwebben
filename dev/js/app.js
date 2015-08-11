6f63d92d13e8ad6e3965dfbe6b3e843515122711
raubas <r.svartholm@gmail.com>
2015-08-11 16:13:40 +0200

    login for dev

--- dev/js/app.js
+++ dev/js/app.js
@@ -71,24 +71,9 @@ app.service('userManagement', function($rootScope){
 		return;
 	}
 	
-})
-
-app.directive('dropzone', function(){
-	// Runs during compile
-	return function (scope, element, attrs) {
-    var config, dropzone;
-
-    config = scope[attrs.dropzone];
-
-    // create a Dropzone for the element with the given options
-    dropzone = new Dropzone(element[0], config.options);
-
-    // bind the given event handlers
-    angular.forEach(config.eventHandlers, function (handler, event) {
-      dropzone.on(event, handler);
-    });
-  };
 });
+
+
 
 app.directive('emitWhen', function () {
     return {
