a92585d6cd7c69dae2037435778b178cd589c653
Jonatan Moritz <jonatan.moritz@gmail.com>
2015-09-01 13:01:14 +0200

    merge fix

--- dev/js/controllers/kartorController.js
+++ dev/js/controllers/kartorController.js
@@ -135,9 +135,9 @@ app.controller('uploadMapCtrl', function ($scope, $filter,$modal, uiGmapGoogleMa
 		});
 	};
 
-<<<<<<< HEAD
+
 	// FILE UPLOAD //
-=======
+
 	// Saves the new mapfile when the file is uploaded
 	$scope.uploadFile = function(files) {
 		var mapfile = new Parse.File("map.pdf", files[0]);
@@ -152,11 +152,11 @@ app.controller('uploadMapCtrl', function ($scope, $filter,$modal, uiGmapGoogleMa
 			console.log(error);
 		});
 	};
->>>>>>> fd8abfe4937e18559a430b325a20c20d171b7111
+
 
 	
 
-<<<<<<< HEAD
+
     $scope.onFileSelect = function($file) {  
         if ($file != null) {
         	console.log($file);
@@ -183,8 +183,7 @@ app.controller('uploadMapCtrl', function ($scope, $filter,$modal, uiGmapGoogleMa
 	// $scope.uploadFile = function(files) {
 	
 	// };
-=======
->>>>>>> fd8abfe4937e18559a430b325a20c20d171b7111
+
 
 	// Save uploaded map to area, and call updateAreaMaps
 	$scope.saveMap = function(area) {
@@ -332,13 +331,11 @@ app.controller('uploadMapCtrl', function ($scope, $filter,$modal, uiGmapGoogleMa
 	//Init function
 	init();
 
-<<<<<<< HEAD
-=======
 	//Remove watchers when view is unloaded
 	$scope.$on("$destroy", function(){
         watchClick();
         //markerService.removeDraggableMarker();
     });
->>>>>>> fd8abfe4937e18559a430b325a20c20d171b7111
+
 
 });
