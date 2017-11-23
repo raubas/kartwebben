aee05dd53f8e3e33150e9f3391b8578b06ad4e93
raubas <r.svartholm@gmail.com>
2017-11-24 00:20:23 +0100

    Upload some stuff

--- dist/js/shims/base.js
+++ dist/js/shims/base.js
@@ -24,7 +24,7 @@
 $(function() {
     $('body').vegas({
         slides: [
-            { src: '/dev/images/skylines_blur.jpg' }
+            { src: '/images/skylines_blur.jpg' }
         ]
     });
 });
